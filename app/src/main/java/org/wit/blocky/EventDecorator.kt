58004b9614b0dbd928b2a164f8573c31f81c422b
dimitraz <dimitrazuccarelli@gmail.com>
2019-02-04 14:54:50 +0000

    Add material calendar view

---
+++ app/src/main/java/org/wit/blocky/EventDecorator.kt
@@ -0,0 +1,19 @@
+package org.wit.blocky
+
+import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+
+
+class EventDecorator(private val color: Int, dates: Collection<CalendarDay>) : DayViewDecorator {
+    private val dates: HashSet<CalendarDay> = HashSet(dates)
+
+    override fun shouldDecorate(day: CalendarDay): Boolean {
+        return dates.contains(day)
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(DotSpan(5f, color))
+    }
+}
--- app/src/main/java/org/wit/blocky/MainActivity.kt
+++ app/src/main/java/org/wit/blocky/MainActivity.kt
@@ -1,12 +1,20 @@
 package org.wit.blocky
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calendarView.setOnDateChangedListener { widget, date, selected ->
+            calendarView.addDecorator(
+                EventDecorator(R.color.colorPrimary, listOf(calendarView.selectedDate))
+            )
+        }
     }
+
 }
