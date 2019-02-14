a2133daed7cecde077916d1126a6ea66756bb3be
dimitraz <dimitrazuccarelli@gmail.com>
2019-02-14 13:41:15 +0000

    Update dummy entries

--- app/src/main/java/org/wit/blocky/views/home/HomeViewModel.kt
+++ app/src/main/java/org/wit/blocky/views/home/HomeViewModel.kt
@@ -1,38 +1,60 @@
 package org.wit.blocky.views.home
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import org.wit.blocky.main.MainApp
 import org.wit.blocky.models.JournalEntry
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
     val allEntries: MutableList<JournalEntry> = mutableListOf()
     val entries: MutableList<JournalEntry> = mutableListOf()
+    var categories: MutableList<String> = mutableListOf()
+    var app = application as MainApp
 
     init {
-        for (i in 1..5) {
-            allEntries.add(
-                JournalEntry(
-                    title = "New Entry $i",
-                    bookmarked = true,
-                    notes = "$i Test entry",
-                    date = CalendarDay.today(),
-                    image = "https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/Taiwan_16x9.jpg?itok=uXcThZKL",
-                    category = "Lifestyle"
-                )
+        allEntries.add(
+            JournalEntry(
+                title = "New Entry 1",
+                bookmarked = true,
+                notes = "1 Test entry",
+                date = CalendarDay.today(),
+                image = "https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/Taiwan_16x9.jpg?itok=uXcThZKL",
+                category = app.categories[1]
             )
-        }
+        )
 
-        for (i in 5..10) {
-            allEntries.add(
-                JournalEntry(
-                    title = "New Entry $i",
-                    bookmarked = true,
-                    notes = "$i Test entry",
-                    date = CalendarDay.today(),
-                    category = "Lifestyle"
-                )
+        allEntries.add(
+            JournalEntry(
+                title = "New Entry 2",
+                bookmarked = true,
+                notes = "2 Test entry",
+                date = CalendarDay.from(2019, 10, 3),
+                category = app.categories[0]
             )
-        }
+        )
+
+        allEntries.add(
+            JournalEntry(
+                title = "New Entry 3",
+                bookmarked = true,
+                notes = "3 Test entry",
+                date = CalendarDay.from(2018, 3, 4),
+                category = app.categories[3]
+            )
+        )
+
+        allEntries.add(
+            JournalEntry(
+                title = "New Entry 4",
+                bookmarked = true,
+                notes = "4 Test entry",
+                date = CalendarDay.from(2018, 2, 2),
+                category = app.categories[3],
+                image = "hello"
+            )
+        )
+
 
         entries.addAll(allEntries)
     }
