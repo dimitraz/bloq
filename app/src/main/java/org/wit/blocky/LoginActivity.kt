24b4d253168ad356b1015ce65b2b29d3d505baa4
dimitraz <dimitrazuccarelli@gmail.com>
2019-03-27 12:29:54 +0000

    Google sign in

--- app/src/main/java/org/wit/blocky/LoginActivity.kt
+++ app/src/main/java/org/wit/blocky/LoginActivity.kt
@@ -1,24 +1,49 @@
 package org.wit.blocky
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 import org.wit.blocky.main.MainApp
 import org.wit.blocky.models.store.FirebaseStore
 
 class LoginActivity : AppCompatActivity() {
 
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var fireStore: FirebaseStore
+    private lateinit var auth: FirebaseAuth
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         app = application as MainApp
+        fireStore = app.entries
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+
+        googleSignIn.setOnClickListener {
+            doSignIn()
+        }
 
         signUp.setOnClickListener {
             val email = userEmail.text.toString()
@@ -43,19 +68,42 @@ class LoginActivity : AppCompatActivity() {
         progressBar.visibility = View.GONE
     }
 
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                googleSignIn(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.i("Bloq", "Google sign in failed: $e")
+            }
+        }
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            Log.i("Bloq", "signed out")
+        }
+    }
+
+    private fun doSignIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
     private fun doLogin(email: String, password: String) {
         showProgress()
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                showToast("Login successful")
-                fireStore.fetchEntries {
-                    hideProgress()
-                    //startActivity(Intent(this, MainActivity::class.java))
-                }
-            } else {
-                showToast("${task.exception?.message}")
-            }
-            hideProgress()
+            doResult(task)
         }
 
     }
@@ -87,5 +135,34 @@ class LoginActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         )
         toast.show()
+    }
+
+    private fun googleSignIn(acct: GoogleSignInAccount) {
+        Log.d("Bloq", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                doResult(task)
+            }
+
+    }
+
+    private fun doResult(task: Task<AuthResult>) {
+        if (task.isSuccessful) {
+            showToast("Login successful")
+            fireStore.fetchEntries {
+                hideProgress()
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        } else {
+            showToast("${task.exception?.message}")
+        }
+        hideProgress()
+    }
+
+    companion object {
+        private const val TAG = "Bloq"
+        private const val RC_SIGN_IN = 9001
     }
 }
--- app/src/main/java/org/wit/blocky/views/profile/ProfileFragment.kt
+++ app/src/main/java/org/wit/blocky/views/profile/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package org.wit.blocky.views.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.wit.blocky.R
 
 class ProfileFragment : Fragment() {
@@ -27,5 +30,26 @@ class ProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+
+        val currentUser= FirebaseAuth.getInstance().currentUser
+        Log.i("Bloq", "Current user: $currentUser")
+
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+
+            Log.i("Bloq", "$name, $email, $photoUrl, $emailVerified, $uid")
+        }
     }
 }
