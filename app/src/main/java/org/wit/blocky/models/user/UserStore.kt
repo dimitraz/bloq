598380d943aefd187a6f70f8c7a5b2e53af999ad
dimitraz <dimitrazuccarelli@gmail.com>
2019-04-01 19:38:17 +0100

    Update firebase user store

--- app/src/main/java/org/wit/blocky/models/user/UserStore.kt
+++ app/src/main/java/org/wit/blocky/models/user/UserStore.kt
@@ -2,7 +2,8 @@ package org.wit.blocky.models.user
 
 interface UserStore {
     fun findAll(): List<UserModel>
-    fun findById(id: String): UserModel?
+    fun findByAuthId(id: String): UserModel?
+    fun findByFbId(id: String): UserModel?
     fun create(entry: UserModel)
     fun update(entry: UserModel)
     fun delete(entry: UserModel)
