9a1bb05a85602720f9a1993dcfcc1ab12d19b24d
dimitraz <dimitrazuccarelli@gmail.com>
2019-04-01 13:22:06 +0100

    Clean up navigation drawer

--- app/src/main/java/org/wit/blocky/MainActivity.kt
+++ app/src/main/java/org/wit/blocky/MainActivity.kt
@@ -1,64 +1,38 @@
 package org.wit.blocky
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import org.wit.blocky.main.MainApp
 
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    lateinit var app: MainApp
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        app = application as MainApp
 
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-
-        val navController = host.navController
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.destination_home),
-            drawer_layout
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        navController.addOnDestinationChangedListener { _, _, _ ->
-            if (drawer_layout.isDrawerOpen(GravityCompat.START))
-                drawer_layout.closeDrawer(GravityCompat.START)
-        }
+        setupNavigation()
     }
 
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        menuItem.isChecked = true
-        drawer_layout.closeDrawers()
 
-        val id = menuItem.itemId
-        when (id) {
-//            R.id.first -> navController.navigate(R.id.firstFragment)
-//            R.id.second -> navController.navigate(R.id.secondFragment)
-//            R.id.third -> navController.navigate(R.id.thirdFragment)
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController, drawer_layout)
+
+        // Handle nav drawer item clicks
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawer_layout.closeDrawers()
+            true
         }
-        return true
 
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawer_layout)
+        // Tie nav graph to items in nav drawer
+        setupWithNavController(navigationView, navController)
     }
 
     override fun onBackPressed() {
@@ -69,8 +43,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
-        return true
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(findNavController(R.id.nav_host_fragment), drawer_layout)
     }
+
 }
