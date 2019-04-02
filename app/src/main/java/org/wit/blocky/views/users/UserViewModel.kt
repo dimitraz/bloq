2472fdc66624ab0959f68884185a51cc7746f0ef
dimitraz <dimitrazuccarelli@gmail.com>
2019-04-02 18:13:13 +0100

    User recycler view

--- app/src/main/java/org/wit/blocky/views/users/UserViewModel.kt
+++ app/src/main/java/org/wit/blocky/views/users/UserViewModel.kt
@@ -1,6 +1,28 @@
 package org.wit.blocky.views.users
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.wit.blocky.main.MainApp
+import org.wit.blocky.models.user.UserModel
 
-class UserViewModel : ViewModel() {
+class UserViewModel(application: Application) : ViewModel() {
+    var app = application as MainApp
+    var users = MutableLiveData<List<UserModel>>()
+
+    init {
+        app.users.fetchUsers {
+            Log.d("Bloq", "From user view model: ${app.users.users}")
+            users.value = app.users.users
+        }
+    }
+}
+
+class UserViewModelFactory(private val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return UserViewModel(application) as T
+    }
 }
