a514b1eddfe5e0b7bc1b1d6cfef756a84eaf15aa
dimitraz <dimitrazuccarelli@gmail.com>
2019-04-19 09:36:33 +0100

    Select tag on entry page

--- app/src/main/java/org/wit/blocky/views/entry/EntryFragment.kt
+++ app/src/main/java/org/wit/blocky/views/entry/EntryFragment.kt
@@ -1,10 +1,15 @@
 package org.wit.blocky.views.entry
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -20,6 +25,13 @@ import org.wit.blocky.adapters.PromptAdapter
 import org.wit.blocky.databinding.FragmentEntryBinding
 import org.wit.blocky.main.MainApp
 import org.wit.blocky.models.CalendarDate
+import android.R.layout.simple_spinner_dropdown_item
+import android.R.layout.simple_spinner_item
+import android.R.array
+import android.widget.Spinner
+
+
+
 
 class EntryFragment : Fragment() {
 
@@ -69,12 +81,53 @@ class EntryFragment : Fragment() {
 
         if (app.currentUser.authId != viewModel.entry!!.authorId) {
             choose_image.visibility = View.GONE
+            category.visibility = View.GONE
             checkBox.visibility = View.GONE
             secondaryToolbar.visibility = View.GONE
         } else {
             // Select entry image
             choose_image.setOnClickListener {
                 viewModel.selectImage(this)
+            }
+
+            var selectedItem = -1
+            val items = app.currentUser.tags
+            val adapter = object : ArrayAdapter<String>(activity, simple_spinner_dropdown_item, items) {
+                override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                    var v: View? = null
+                    v = super.getDropDownView(position, null, parent)
+                    // If this is the selected item position
+                    if (position == selectedItem) {
+                        v!!.setBackgroundColor(Color.LTGRAY)
+                    } else {
+                        // for other views
+                        v!!.setBackgroundColor(Color.WHITE)
+
+                    }
+                    return v
+                }
+            }
+            category.adapter = adapter
+
+            if (viewModel.entry!!.category.isNotEmpty()) {
+                val pos = adapter.getPosition(viewModel.entry!!.category)
+                category.setSelection(pos)
+            }
+
+            category.onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    val tag = parent.getItemAtPosition(position) as String
+                    Log.v("Bloq", "Selected tag: $tag")
+                    selectedItem = position
+                    viewModel.entry!!.category = tag
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {}
             }
 
             secondaryToolbar.inflateMenu(R.menu.menu_entry)
